2630d220618eb455e2234549fad6057c890244e1
nadir inab <nadir.inab.dev@gmail.com>
2023-11-16 14:02:03 +0100

    inffra is well done

--- src/main/java/com/youcode/rentalhive/RentalHiveApplication.java
+++ src/main/java/com/youcode/rentalhive/RentalHiveApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RentalHiveApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RentalHiveApplication.class, args);
     }
 
 }
+
---
+++ src/main/java/com/youcode/rentalhive/infrastructure/user/UserController.java
@@ -0,0 +1,4 @@
+package com.youcode.rentalhive.infrastructure.user;
+
+public interface UserController {
+}
