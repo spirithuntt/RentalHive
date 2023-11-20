6caf9f0851834587422847457d75dc1d99d2b14e
Nouhaila El Housni <112879456+spirithuntt@users.noreply.github.com>
2023-11-20 08:47:49 +0100

    Add Equipment Structure

--- src/main/java/com/youcode/rentalhive/domain/equipement/EquipementRepository.java
+++ src/main/java/com/youcode/rentalhive/domain/equipement/EquipementRepository.java
@@ -1,4 +1,11 @@
 package com.youcode.rentalhive.domain.equipement;
 
-public class EquipementRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EquipementRepository extends JpaRepository<Equipement, Long>{
+    public Equipement findByNameAndPlateNumber(String name, UUID plateNumber);
 }
