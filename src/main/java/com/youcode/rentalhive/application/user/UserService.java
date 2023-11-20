147107c6ea809e20bcfed2345ed5fdd57718df5a
nadir inab <nadir.inab.dev@gmail.com>
2023-11-20 11:19:07 +0100

    user service is well added

--- src/main/java/com/youcode/rentalhive/application/user/UserService.java
+++ src/main/java/com/youcode/rentalhive/application/user/UserService.java
@@ -1,4 +1,10 @@
 package com.youcode.rentalhive.application.user;
 
+
+import com.youcode.rentalhive.domain.user.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface UserService {
+    public User save(User user) ;
 }
