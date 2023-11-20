9ac46a78252e792516968db4498d0f20916fb424
ysres <yas.esraidi@gmail.com>
2023-11-20 16:15:18 +0100

    Create search by equipementId and userId

--- src/main/java/com/youcode/rentalhive/domain/reservation/ReservationRepository.java
+++ src/main/java/com/youcode/rentalhive/domain/reservation/ReservationRepository.java
@@ -1,15 +1,17 @@
 package com.youcode.rentalhive.domain.reservation;
 
+import com.youcode.rentalhive.domain.equipement.Equipement;
 import com.youcode.rentalhive.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    public Reservation findByIdAndUserEquals(Long id , User userId);
+    public List<Reservation> findByEquipementIdAndUserId(Long equipement , Long user);
 
 }
--- src/main/java/com/youcode/rentalhive/domain/user/UserRepository.java
+++ src/main/java/com/youcode/rentalhive/domain/user/UserRepository.java
@@ -3,5 +3,6 @@ package com.youcode.rentalhive.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface UserRepository  {
+@Repository
+public interface UserRepository  extends JpaRepository<User,Long>{
 }
