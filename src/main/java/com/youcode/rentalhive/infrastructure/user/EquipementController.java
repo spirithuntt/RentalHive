7d41b2036a8c592763e41325aefcfe78e2393263
Nouhaila El Housni <112879456+spirithuntt@users.noreply.github.com>
2023-11-20 15:19:35 +0100

    Add Validation and Endpoints for Equipment

---
+++ src/main/java/com/youcode/rentalhive/infrastructure/user/EquipementController.java
@@ -0,0 +1,16 @@
+package com.youcode.rentalhive.infrastructure.user;
+
+import com.youcode.rentalhive.domain.equipement.Equipement;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface EquipementController {
+
+    Equipement saveEquipement(Equipement equipement);
+
+    List<Equipement> getAllEquipements();
+
+    Equipement getEquipementByNameAndUuid(String name, UUID uuid);
+
+}
---
+++ src/main/java/com/youcode/rentalhive/infrastructure/user/EquipementControllerImp.java
@@ -0,0 +1,36 @@
+package com.youcode.rentalhive.infrastructure.user;
+
+import com.youcode.rentalhive.application.equipement.EquipementService;
+import com.youcode.rentalhive.domain.equipement.Equipement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/equipements")
+public class EquipementControllerImp implements EquipementController{
+
+        private final EquipementService equipementService;
+
+        @Autowired
+        public EquipementControllerImp(EquipementService equipementService) {
+            this.equipementService = equipementService;
+        }
+
+        @PostMapping
+        public Equipement saveEquipement(@RequestBody Equipement equipement) {
+            return equipementService.save(equipement);
+        }
+
+        @GetMapping
+        public List<Equipement> getAllEquipements() {
+            return equipementService.findAll();
+        }
+
+        @GetMapping("/{name}/{uuid}")
+        public Equipement getEquipementByNameAndUuid(@PathVariable String name, @PathVariable UUID uuid) {
+            return equipementService.findByNameAndUuid(name, uuid);
+        }
+}
